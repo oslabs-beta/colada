ec0c8d9195fdb7d08613ac32f12d4731535b8dfe
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-09-07 19:16:19 -0700

    moved content script to own file, injected statically

--- colada-extension/devtools-background.js
+++ colada-extension/devtools-background.js
@@ -1,68 +1,14 @@
-"use strict"
+// loaded by devtools-background.html, which is the the "background" page for the devtools-panel
+// this is declared in manifest.json. note that this does not actually represent the UI, that is the devtools "panel") 
 
-async function onShownListener() {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: devPanelScript,
-  });
-};
-
-// function messageListener(event) {
-//   if (event.data.source === "colada") {
-//     console.log('message received from plugin...');
-//     const date = Date.now().toString();
-//     chrome.storage.local.set({ [date] : event.data.payload}, () => {
-//       console.log('event data saved at key ', date)
-//     });
-
-//   }
-// }
+// async function onShownListener() {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     func: devPanelScript,
+//   });
+// };
 
 chrome.devtools.panels.create(
-  'Colada DevTools',
-  '',
-  'devtools-panel.html',
-  () => {
-    console.log("panel created at", new Date());
-    onShownListener();
-  }
+  'Colada DevTools', '', 'devtools-panel.html', () => onShownListener()
 );
-
-function devPanelScript() {
-  console.log("running injected devPanelScript...");
-
-  function ping() {
-    chrome.runtime.sendMessage('ping', response => {
-      if (chrome.runtime.lastError) {
-        console.log('trying to connect to chrome runtime again')
-        setTimeout(ping, 1000);
-      } else {
-        console.log('no more runtime last error')
-        chrome.runtime.onDisconnect.addListener(handleDisconnect) 
-      }
-    });
-  }
-  
-  ping();
-
-  function messageListener(event) {
-    if (event.data.source === "colada") {
-      console.log('message received from plugin...');
-      const date = Date.now().toString();
-      chrome.storage.local.set({ [date] : event.data.payload}, () => {
-        console.log('event data saved at key ', date)
-      });
-    }
-  };
-
-  function handleDisconnect() {
-    console.log('handle disconnect')
-    window.removeEventListener("message", messageListener);
-  }
-
-  window.addEventListener("message", messageListener);
-  
-  }
-
-  
--- colada-extension/popup.js
+++ colada-extension/popup.js
@@ -1,8 +1,10 @@
-window.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {},
-});
-});
+// not in use, just saved as an example
+//
+// window.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     func: () => {},
+// });
+// });
