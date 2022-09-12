37e8eed039d480bc61bf8e9e81e1a7ed6d3f4711
Parker Steinberg <parker@Parkers-MBP-2.lan>
2022-09-12 10:17:13 -0700

    comments for inspector and timeline

--- colada-plugin/src/ColadaDevToolsPlugin/inspector.ts
+++ colada-plugin/src/ColadaDevToolsPlugin/inspector.ts
@@ -1,1 +1,15 @@
 export {}
+
+// add our custom inspector
+// API: api.addInspector
+// triggered upon page load
+
+// add elements to inspector
+// API: api.on.getInspectorTree
+// iterate over labels for each store to add to inspector
+
+// add state to inspector
+// API: api.on.getInspectorState 
+// iterate over most recent versions of stores and add all that data to the inspector 
+
+// stretch: add tags to component inspector?
--- colada-plugin/src/ColadaDevToolsPlugin/timeline.ts
+++ colada-plugin/src/ColadaDevToolsPlugin/timeline.ts
@@ -1,1 +1,16 @@
 export{}
+
+// add timeline layer
+// API: api.addTimelineLayer
+
+// add event to timeline when:
+// 1. page initially loads 
+// 2. $subscribe is invoked (emits "addTimlineEvent" event)
+// API: addTimelineEvent
+
+// unsubscribe? after event is added
+// problem is that when click on a timelineEvent it changes the state, which triggers $subscribe, which adds another timeline event 
+// so when a user clicks on the timeline event, we need to: 
+// 1. change the app's state
+// 2. unsubscibe from stores (maybe do this first?)
+// 3. resubscibe to all stores 
