3e0b8098babcb8776cf7b902bc1db262895a46f3
Daniel Steinbrook <danielsteinbrook@Daniels-MacBook-Pro.local>
2022-09-12 18:08:26 -0400

    added html to webpack

--- colada-extension/webpack.mix.js
+++ colada-extension/webpack.mix.js
@@ -1,4 +1,12 @@
 let mix = require('laravel-mix');
+let fs = require('fs');
+
+mix.before(() => {fs.copyFileSync('./assets/html/popup.html','dist/popup.html');
+                  fs.copyFileSync('./assets/html/devtools-panel.html','dist/devtools-panel.html');
+                  fs.copyFileSync('./assets/html/devtools-background.html','dist/devtools-background.html');
+                  fs.copyFileSync('./manifest.json','dist/manifest.json');
+
+                            });
 
 mix.setPublicPath('./')
     .sass('app-frontend/src/assets/style.scss', 'dist/css')
@@ -10,3 +18,4 @@ mix.setPublicPath('./')
     .options({
         processCssUrls: false
     });
+
