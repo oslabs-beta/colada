8f4711f61a5f9b386da1fcf6afa212e9c64cb5d1
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-09-12 15:23:07 -0700

    implemented deep clone on snapshots

--- colada-plugin/src/ColadaDevToolsPlugin/stateHandler.ts
+++ colada-plugin/src/ColadaDevToolsPlugin/stateHandler.ts
@@ -1,4 +1,5 @@
 import { piniaStores } from "../PiniaColadaPlugin/index"
+import * as _ from "lodash"
 
 // create storeHistory array and type it
 const storeHistory: any = [];
@@ -6,24 +7,25 @@ const storeHistory: any = [];
 const handleStoreChange = (snapshot: any) => {
   
   console.log('handling store change')
+  const snapshotClone = _.cloneDeep(snapshot)
 
   // push to storeCache the updated state (which is the state argument)
   storeHistory.push({
-    [snapshot.timestamp]: {
-      state: {...snapshot.value}
+    [snapshotClone.timestamp]: {
+      [snapshotClone.key]: snapshotClone
     }
   })
 
   //we want to create a new timeline event
   //emit a custom event with the proxyObj as a payload
-  const event: any = new CustomEvent('addTimelineEvent', {detail: {...snapshot}})
+  const event: any = new CustomEvent('addTimelineEvent', {detail: snapshotClone})
   window.dispatchEvent(event)
 
   //post a message with the piniaObjs as the payload
   //send a messsage to the window for the extension to make use of
   const messageObj: any = {
     source: 'colada',
-    payload: snapshot
+    payload: snapshotClone
   }
   window.postMessage(JSON.stringify(messageObj), "http://localhost:5173")   
 
@@ -36,9 +38,9 @@ const getState = () => {
   piniaStores.subscribe(handleStoreChange)
 }
 
-// create getter to access a specified snapshot from storeHistory for time travelling
+// create getter to access a specified snapshotClone from storeHistory for time travelling
 
-// create getter to access a the MOST RECENT snapshot from storeHistory for inspector
+// create getter to access a the MOST RECENT snapshotClone from storeHistory for inspector
 
 export {
   getState
