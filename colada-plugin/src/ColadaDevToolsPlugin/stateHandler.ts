66f2e20b0cbc57ed8528f8020a8b2bc301b175bf
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-09-16 19:41:19 -0700

    add eslint and run linting+formatting in plugin dir

--- colada-plugin/src/ColadaDevToolsPlugin/stateHandler.ts
+++ colada-plugin/src/ColadaDevToolsPlugin/stateHandler.ts
@@ -1,9 +1,9 @@
-import { piniaStores } from "../PiniaColadaPlugin/index"
-import * as _ from "lodash"
+import { piniaStores } from '../PiniaColadaPlugin/index';
+import * as _ from 'lodash';
 
 // delcare global variables
 const storeHistory: any = [];
-declare const window: any 
+declare const window: any;
 let combinedSnapshot: any = {};
 const storeLabels: any = [];
 
@@ -18,8 +18,8 @@ const outputCombinedSnapshot = _.debounce(() => {
   console.log(`outputCombinedSnapshot running at time ${Date.now()}`);
   // delcare variable missing stores, which will have the labels for the missing stores from snapShot
   const missingStores = storeLabels.filter((label: any) => {
-    return !Object.keys(combinedSnapshot[Object.keys(combinedSnapshot)[0]]).includes(label)
-  })
+    return !Object.keys(combinedSnapshot[Object.keys(combinedSnapshot)[0]]).includes(label);
+  });
   // iterate over missing stores, find the corresponding most recent snapshot, and add to combinedSnapshot
   missingStores.forEach((store: any) => {
     // can replace this with getter function below
@@ -27,98 +27,98 @@ const outputCombinedSnapshot = _.debounce(() => {
     const mostRecentSnapshot = getCurrentStores(true);
     const mostRecentSnapshotClone: any = _.cloneDeep(mostRecentSnapshot);
     // get correspong store and have to const
-    const mostRecentStore = mostRecentSnapshotClone[Object.keys(mostRecentSnapshotClone)[0]][store]
+    const mostRecentStore = mostRecentSnapshotClone[Object.keys(mostRecentSnapshotClone)[0]][store];
     // add hasBeenUpdated = false property to snapshot we're adding
     mostRecentStore.hasBeenUpdated = false;
     // add to combinedSnapshot
     combinedSnapshot[Object.keys(combinedSnapshot)[0]][store] = mostRecentStore;
-  })
-  
+  });
+
   // pushing combinedSnap to storeHistory, triggering custom event, and posting message to window 
-  storeHistory.push(combinedSnapshot)
+  storeHistory.push(combinedSnapshot);
   //emit a custom event with the proxyObj as a payload
-  const event: any = new CustomEvent('addTimelineEvent', {detail: combinedSnapshot})
-  window.dispatchEvent(event)
+  const event: any = new CustomEvent('addTimelineEvent', { detail: combinedSnapshot });
+  window.dispatchEvent(event);
 
   //send a messsage to the window for the extension to make use of
   const messageObj: any = {
     source: 'colada',
     payload: combinedSnapshot
-  }
+  };
   // TODO: change the second argument here to current URL?
-  window.postMessage(JSON.stringify(messageObj), "http://localhost:5173")
+  window.postMessage(JSON.stringify(messageObj), 'http://localhost:5173');
 
-  console.log('storeHistory is...........:', storeHistory)
+  console.log('storeHistory is...........:', storeHistory);
   // reset combinedSnapshot to empty object
   combinedSnapshot = {};
-}, 10)
+}, 10);
 
 const handleStoreChange = (snapshot: any) => {
-  
-  console.log(`handleStoreChange running at ${Date.now()}`)
-  const snapshotClone = _.cloneDeep(snapshot)
 
-  console.log('snapshot in handleStoreChange.....', snapshot)
+  console.log(`handleStoreChange running at ${Date.now()}`);
+  const snapshotClone = _.cloneDeep(snapshot);
+
+  console.log('snapshot in handleStoreChange.....', snapshot);
 
   // add hasBeenUpdated property to true on snapshotClone
   snapshotClone.hasBeenUpdated = true;
 
   // add snapshots's label ('key' proprety) to storeLabels if it's not already in there
   // ! really, this only needs to run on initial page load but it will end up checking the conditional on every trigger of handleStoreChange
-  if (!storeLabels.includes(snapshotClone.key)){
-    storeLabels.push(snapshotClone.key)
+  if (!storeLabels.includes(snapshotClone.key)) {
+    storeLabels.push(snapshotClone.key);
   }
 
   // if finalSnaphsot has no properites, add initial timestamp property along with associated snapshotClone
   if (_.isEmpty(combinedSnapshot)) {
     combinedSnapshot[snapshotClone.timestamp] = {
-        [snapshotClone.key]: snapshotClone
-      }
+      [snapshotClone.key]: snapshotClone
+    };
   }
   // else, add a new key to combinedSnapshot at existing timestamp property
   else {
-    combinedSnapshot[Object.keys(combinedSnapshot)[0]][snapshotClone.key] = snapshotClone
+    combinedSnapshot[Object.keys(combinedSnapshot)[0]][snapshotClone.key] = snapshotClone;
   }
 
-  console.log('storeHistory at end of handleStoreChange', storeHistory)
+  console.log('storeHistory at end of handleStoreChange', storeHistory);
 
   // invoke debounced outputCombinedSnapshot
   outputCombinedSnapshot();
-}
+};
 
 
 // import the subscribe method and implement associated functionality 
 const initializeState = () => {
-  console.log('invoking initializeState!')
-  piniaStores.subscribe(handleStoreChange, true)
-}
+  console.log('invoking initializeState!');
+  piniaStores.subscribe(handleStoreChange, true);
+};
 
 const resubscribe = () => {
-  piniaStores.subscribe(handleStoreChange, false)
-}
+  piniaStores.subscribe(handleStoreChange, false);
+};
 
 const unsubscribe = () => {
-  piniaStores.unsubscribe()
-}
+  piniaStores.unsubscribe();
+};
 
 
 // NOTE: currently 0(n) ... consider refactoring to use binary search
 const getSnapshotbyTimestamp = (timestamp: number) => {
-  console.log('retrieving snapshot at timestamp ', timestamp)
-  for (const e of storeHistory){
+  console.log('retrieving snapshot at timestamp ', timestamp);
+  for (const e of storeHistory) {
     if (parseInt(Object.keys(e)[0]) === timestamp) return e;
-  } 
-}
+  }
+};
 
 const setAppState = (snapshot: any) => {
-  console.log('running setAppState...')
-  console.log('snapshot in setAppState:', snapshot)
-  const stores: any = Object.values(snapshot)[0]
-  for (const key in stores){ 
-    console.log('in for loop in setAppState!')
-    window.store[key].$state = stores[key].value
+  console.log('running setAppState...');
+  console.log('snapshot in setAppState:', snapshot);
+  const stores: any = Object.values(snapshot)[0];
+  for (const key in stores) {
+    console.log('in for loop in setAppState!');
+    window.store[key].$state = stores[key].value;
   }
-}
+};
 
 // create getter to access a specified snapshot from storeHistory for time travelling
 
@@ -127,14 +127,14 @@ const setAppState = (snapshot: any) => {
  @param {boolean} [includeTimestamps=false] - To retrieve data with timestamps. Defaults to false.
 */
 // TODO: decide if we want to use cloneDeep here? vs a reference
-const getCurrentStores = (includeTimestamps: boolean = false) => {
+const getCurrentStores = (includeTimestamps = false) => {
   if (includeTimestamps) {
-    return storeHistory[storeHistory.length - 1]; 
+    return storeHistory[storeHistory.length - 1];
   }
   else {
-    return Object.values(storeHistory[storeHistory.length - 1])[0]
+    return Object.values(storeHistory[storeHistory.length - 1])[0];
   }
-}
+};
 
 export {
   initializeState,
@@ -143,4 +143,4 @@ export {
   setAppState,
   resubscribe,
   unsubscribe
-}
+};
--- colada-plugin/src/PiniaColadaPlugin/index.ts
+++ colada-plugin/src/PiniaColadaPlugin/index.ts
@@ -1,23 +1,23 @@
-import { ProxyObject } from '../types'
+import { ProxyObject } from '../types';
 
-let unsubscribeMethods: Array<() => void> = [];  
-let stores: Array<any> = [];  
-let piniaProxies: ProxyObject[] = [];
+const unsubscribeMethods: Array<() => void> = [];
+const stores: Array<any> = [];
+const piniaProxies: ProxyObject[] = [];
 
 // declare object where methods will be stored and exported
 const piniaStores: any = {};
 
 piniaStores.unsubscribe = () => {
-  console.log('unsubscribing...')
+  console.log('unsubscribing...');
   unsubscribeMethods.forEach(e => e());
-}
+};
 
-piniaStores.getPiniaStores = (): any => { 
-  return piniaProxies
-}
+piniaStores.getPiniaStores = (): any => {
+  return piniaProxies;
+};
 
-piniaStores.subscribe = (callback: any, setInitialState: boolean = false) => {
-  console.log('subscribing...')
+piniaStores.subscribe = (callback: any, setInitialState = false) => {
+  console.log('subscribing...');
   stores.forEach(store => {
     const snapshot: ProxyObject = {
       timestamp: Date.now(),
@@ -28,26 +28,26 @@ piniaStores.subscribe = (callback: any, setInitialState: boolean = false) => {
       getters: store._hmrPayload.getters,
       actions: store._hmrPayload.actions,
       editable: true
-    }
+    };
     const unsubscribeMethod = store.$subscribe(() => {
-      snapshot.timestamp = Date.now()
-      console.log('subscribe callback')
+      snapshot.timestamp = Date.now();
+      console.log('subscribe callback');
       // we can also access mutation and state here
-      callback(snapshot)
-    })
-    unsubscribeMethods.push(unsubscribeMethod)
-    if (setInitialState) callback(snapshot)
-    // callback(snapshot)
-  }) 
-}
+      callback(snapshot);
+    });
+    unsubscribeMethods.push(unsubscribeMethod);
+    if (setInitialState) callback(snapshot);
+  });
+};
+
 
 const PiniaColadaPlugin = (context: any) => {
-  console.log("devtools.js PiniaColadaPlugin context.store: ", context.store)
-  const store: any = context.store
+  console.log('devtools.js PiniaColadaPlugin context.store: ', context.store);
+  const store: any = context.store;
   stores.push(store);
-}
+};
 
 export {
   piniaStores,
   PiniaColadaPlugin
-}
+};
